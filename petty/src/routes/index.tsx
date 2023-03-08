136f55d1bf2dff9b705817162dd6274cb35910b4
alejandrosilveiraramos <alejandrosilveiraramos3@gmail.com>
2023-03-08 13:39:09 -0300

    feat: add react router dom

---
+++ petty/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path='/home' element={<p>Home</p>}/> 
+
+            <Route path='*' element={<Navigate to='/home' />} />
+        </Routes>
+    )
+
+}
